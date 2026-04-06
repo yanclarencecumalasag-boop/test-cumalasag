2c4b9c1342c24fa545814c5ffae073793dca571b
yanclarencecumalasag-boop <yanclarencecumalasag@gmail.com>
2026-04-06 19:06:56 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require 'db.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+    $stmt->execute([$username, $password]);
+
+    if($stmt->rowCount() > 0){
+        $_SESSION['user'] = $username;
+        header("Location: dashboard.php");
+        exit();
+    }else{
+        $error = "Invalid Username or Password";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Ian Clarence Cumalasag</h2>
+
+    <?php if (!empty($error)) echo "<p style = 'color:red'>$error</p>"?>
+    
+    <form method="POST">
+        <label>User Name:</label><br>
+        <input type="text" name="username"><br>
+        <label>Password:</label><br>
+        <input type="password" name="password"><br>
+        <button type="">Login</button>
+    </form>    
+</body>
+</html>
+
